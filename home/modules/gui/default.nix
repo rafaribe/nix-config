f5cb0804dbd0fbd1a5386edaa30d225e412495fd
rafaribe <rafael.ntw@gmail.com>
2024-07-09 19:15:19 +0100

    feat: not dirty anymore

--- home/modules/gui/default.nix
+++ home/modules/gui/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ./media.nix
     ./productivity.nix
+    ./utils.nix
   ];
 }
--- home/modules/xdg.nix
+++ home/modules/xdg.nix
@@ -1,4 +1,18 @@
 {...}: {
   # for electron apps
   home.sessionVariables.DEFAULT_BROWSER = "brave-browser.desktop";
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = let
+      browser = "brave-browser.desktop";
+    in {
+      "text/html" = browser;
+      "x-scheme-handler/http" = browser;
+      "x-scheme-handler/https" = browser;
+      "x-scheme-handler/about" = browser;
+      "x-scheme-handler/unknown" = browser;
+    };
+  };
 }
+  
